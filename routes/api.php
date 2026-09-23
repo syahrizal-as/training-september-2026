f8340bfa49f29660c5e758c250634b5b6d3dce49
syahrizal_as <alisadikinsyahrizal@gmail.com>
2026-09-23 20:29:14 +0700

    feat: add Laravel Sanctum authentication and Scramble API documentation

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+Route::post('logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
